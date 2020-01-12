285a4e7f5ab0cb91cb2df5e4900f19c380809cb0
Austin Smith <smithaustin@users.noreply.github.com>
2020-01-12 08:54:22 -0800

    Graph comps (#3)
    * added temperature
    
    * Only high working
    
    * changing file styles

--- frontend/src/components/SideVisual.js
+++ frontend/src/components/SideVisual.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import {
     LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, AreaChart, Area,
 } from 'recharts';
+import TemperatureGraph from "./TemperatureGraph";
 
 
 export class SideVisual extends Component {
@@ -15,55 +16,14 @@ export class SideVisual extends Component {
         }
     }
 
-    componentDidMount(props) {
-        axios.get('data/' + this.props.location + '.json')
-            .then(res => {
-                console.log(res.data)
-                this.setState({
-                    data: res.data
-                })
-
-                console.log(this.state.data.climate.high.map((value, index) => {
-                    return {name: index, t: value}
-                }))
-
-
-            })
-        console.log(this.props.location)
-    }
-
-    componentWillUpdate(){
-        
-    }
-
     render() {
         const location = this.props.location.charAt(0).toUpperCase() + this.props.location.slice(1)
         return (
             <div>
-                <h1 style={{color: "#29066B"}}>{location}</h1>
-                <h2>Population of {this.props.population}</h2>
-                {/* <p>{this.props.population}</p> */}
-
-                <h2 style={{color: "#29066B"}}></h2>
-
-                <div>
-                    {this.state.data && (
-                        <AreaChart
-                            // width={}
-                            height={400}
-                            data={this.state.data.climate.high.map((value, index) => {
-                                return {name: index, t: value}
-                            })}
-                            margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-                        >
-                            <XAxis dataKey="name" />
-                            <Tooltip />
-                            <CartesianGrid stroke="#f5f5f5" />
-                            <Area type="monotone" dataKey="t" stroke="#ff7300" yAxisId={0} fill="#fff000" />
-                            {/* <Area type="monotone" dataKey="t" stroke="#387908" yAxisId={1} /> */}
-                        </AreaChart>
-                    )}
-                </div>
+                <h2>{this.props.location}</h2>
+                <h2>{this.props.industry}</h2>
+                <p>{this.props.population}</p>
+                <TemperatureGraph location={this.props.location}/>
             </div>
         );
     }
