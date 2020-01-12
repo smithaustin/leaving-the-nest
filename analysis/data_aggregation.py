d57ed1f5b5eee7b8a63288da90d3d2687e6bcf18
Austin <smith.austin149@gmail.com>
2020-01-11 22:38:43 -0800

    added constants

--- analysis/data_aggregation.py
+++ analysis/data_aggregation.py
@@ -2,13 +2,18 @@ import pandas as pd
 
 class StatsCan:
 
+    labour = (1899, 1919)
+    commute_time = (1937, 1942)
+    commute_mode = (1930, 1936)
+    population = (0,1)
+
     def __init__(self, population_centre_csv):
         self.population_centres = pd.read_csv(population_centre_csv)
 
-    # use for 
-    def get_pc_attribute(self, df, city, idx_min, idx_max, attrib_name):
+    # see above constants for idxs value 
+    def get_pc_attribute(self, df, city, idxs, attrib_name):
         """ Returns the df of a specific attribute a Population Centre"""
-        attrib_data = df[df['GEO_NAME'] == city].reset_index().iloc[idx_min:idx_max].reset_index()
+        attrib_data = df[df['GEO_NAME'] == city].reset_index().iloc[idxs[0]:idxs[1]].reset_index()
         new_df = pd.DataFrame()
         new_df[attrib_name] = attrib_data["DIM: Profile of Population Centres (2247)"]
         new_df[attrib_name + "_count"] = attrib_data["Dim: Sex (3): Member ID: [1]: Total - Sex"]
